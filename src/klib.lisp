cd26935b41e5b2bfb7a6f77150cffa143c1a555c
vlad-km <gm5461@gmail.com>
2017-09-07 14:39:28 +0300

    Add

--- src/klib.lisp
+++ src/klib.lisp
@@ -1,21 +1,91 @@
-(/debug "Klib...")
+;;; -*- mode:lisp; coding:utf-8 -*-
+;;;
+;;; Klib
+;;;
+;;; Copyright (C) 2017 mvk (github.com/vlad-km)
+;;;
 
-;;;(unless #j:opNew
-;;;    (#j:console:log "klib runtime not present")
-;;;    (error "klib runtime not present") )
+;;;
+;;; addon *features* support
+;;;
+;;; Another addon should check *features* and make a decision about the continuation
+;;; with the issuance of diagnostics
+;;;
+;;; :release may be list => '(pre-01 pre-02 pre-0.1)
+;;;
+;;; (addon-provide :tws '(v1 prot-0))
+;;; (addon-provide :tws 'v1)
+;;;
+(defun addon-provide (feature &key release)
+    (let ((f (find feature *features*))
+          (r (if release (ensure-list release) nil)))
+        (cond (f
+               (when r
+                   (setq plist (symbol-plist f))
+                   (if plist
+                       (setf (getf plist :relese) (list :release r))
+                       (setf (symbol-plist f) (list :release r)))))
+              (t
+               (setq f feature)
+               (if r (setf (symbol-plist f) (list :release r)))
+               (push f *features*)))))
 
-(addon-provide :klib)
+(export '(addon-provide))
 
-;;;(/debug "provide")
+
+;;;
+;;; (addon-requiere :trivial-ws :release 'pre-02)
+;;; if existing  ws addon has release pre-01, will be raise error
+;;;
+;;;
+;;; and form (addon-require '(:tws (v1 prot-1 send-0) '(:sock (v1 pre-01))))
+;;; or form  (addon-require '(:tws v1))
+;;;
+(defun addon-require (&rest reqp)
+    (dolist (r reqp)
+        (if (consp r)
+            (addon-require-1 (car r) :release (cadr r))
+            (addon-require-1 r) ))
+    t)
+
+(export '(addon-require))
+
+
+(defun addon-require-1 (feature &key release)
+    (let ((present (find feature *features*)))
+        (unless present
+            (error (concat  " :" (symbol-name feature) " it bug, but not feature")))
+        (let* ((plist (symbol-plist present))
+               (rel-present (getf plist :release))
+               (msg))
+            (when release
+                (cond ((symbolp release)
+                       (if (find release rel-present)
+                           (return-from addon-require-1 t)
+                           (error (concat "Feature " (symbol-name feature) " not " release))))
+                      ((consp release)
+                       (dolist (r release)
+                           (if (find r rel-present)
+                               t
+                               (error (concat "Feature " (symbol-name feature) " not " r)))))
+                      (t (error "wtf feature release ~a ?" release)))
+                t))))
+
+
 
 ;;;
 ;;; Export jscl symbols
 ;;;
 ;;;
 
+
 (export  '(oget new make-new fset def!struct
            concat lisp-to-js  js-to-lisp js-null-p join
            %js-try))
 
-(/debug "DONE")
+
+
+(addon-provide :klib)
+
+
 ;;; EOF
